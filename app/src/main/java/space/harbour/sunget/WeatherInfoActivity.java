90d61d08274397706367f455d85464d94a1f0952
Marius <mariusaklages@gmail.com>
2018-11-25 23:10:51 +0100

    Doc Commit

--- app/src/main/java/space/harbour/sunget/WeatherInfoActivity.java
+++ app/src/main/java/space/harbour/sunget/WeatherInfoActivity.java
@@ -5,12 +5,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+/**
+ * Public class extending Activity enabling the
+ * users to perform an additional activity.
+ * This additional activity is enabling the user
+ * to have more detailed information on the weather
+ * of a specific city.
+ */
 public class WeatherInfoActivity extends Activity {
     private Weather weather;
     private TextView codeTextView;
     private TextView cityTextView;
     private TextView tempTextView;
 
+    /**
+     * This method is used to initialize the activity.
+     * Initially, the content is set to a specific view
+     * by setContentView
+     * Thereafter, we get the  weather data and we
+     * are retrieving objects.
+     * For the ease of the eye we are adding the
+     * corresponding weather status icon to the UI
+     * and initialize additional views.
+     * @param savedInstanceState from a String keys to various values
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
